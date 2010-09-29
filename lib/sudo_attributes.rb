05dcea859485f4673e1a1afb53a0924917ee662e
Peter Brown <pete@lette.us>
2010-09-28 20:35:08 -0400

    Cleaned up code and simplified usage

--- lib/sudo_attributes.rb
+++ lib/sudo_attributes.rb
@@ -1,48 +1,57 @@
 module SudoAttributes
   
-  module InstanceMethods
-    def sudo_update_attributes(new_attributes)
-      self.send(:attributes=, new_attributes, false)
-      save
+  module Base
+    # This is the method that will be called from each model you want to enable SudoAttributes in
+    def enable_sudo_attributes(attrs=nil)
+      raise "Invalid argument passed to enable_sudo_attributes" unless valid_attributes? attrs
+
+      set_protected_attributes(attrs) unless attrs.nil?
+
+      self.send :extend, SudoAttributes::ClassMethods
+      self.send :include, SudoAttributes::InstanceMethods
+    end
+    
+    private
+    
+    # Validate that either nil, :protect, or :accessible has been given
+    def valid_attributes?(attrs)
+      attrs.nil? || (attrs.is_a?(Hash) && (attrs.has_key?(:protected) || attrs.has_key?(:accessible)))
+    end
+
+    # Set the protected attributes depending on the key
+    def set_protected_attributes(attrs)
+      key = attrs.has_key?(:protected) ? :protected : :accessible
+
+      # Call either attr_protected or attr_accessible
+      self.send("attr_#{key}", *attrs[key])
     end
   end
   
+  # Added to ActiveRecord model only if enable_sudo_attributes is called
   module ClassMethods
-    
-    # protected_attributes    
-    def sudo_attr_protected(protected_attrs=nil)
-      self.attr_protected *protected_attrs unless protected_attrs.nil?
-      add_initialize(self)
-    end
-    
-    def sudo_attr_accessible(accessible_attrs=nil)
-      self.attr_accessible *accessible_attrs unless accessible_attrs.nil?
-      add_initialize(self)
-    end
-    
     def sudo_create(attributes=nil)
       instance = sudo_new(attributes)
       instance.save
       instance
     end
-    
+
     def sudo_new(attributes=nil)
       new(attributes, false)
     end
-    
-    private
-    
-    def add_initialize(klass)
-      klass.class_eval do
-        def initialize(attributes=nil, attributes_protected=true)
-          super(nil)
-          send(:attributes=, attributes, attributes_protected)
-        end
-      end
+  end
+ 
+
+  module InstanceMethods
+    def initialize(attributes=nil, attributes_protected=true)
+      super(nil)
+      send(:attributes=, attributes, attributes_protected)
     end
-  
-  end  
+    
+    def sudo_update_attributes(new_attributes)
+      self.send(:attributes=, new_attributes, false)
+      save
+    end
+  end
 end
 
-ActiveRecord::Base.send :extend, SudoAttributes::ClassMethods
-ActiveRecord::Base.send :include, SudoAttributes::InstanceMethods
+ActiveRecord::Base.send :extend, SudoAttributes::Base
